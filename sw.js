a96f5e36dfb9000d4ad349eade3f87ea2375dba6
Agro-Polo <patryk96pol@gmail.com>
2026-08-26 23:15:51 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='agromanager-v13-2';
+const CACHE='agromanager-v13-4';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./version.json'];
 
 self.addEventListener('install',event=>{
